69b36d80c930a6e647a3462c617a668d41f9cb82
Aman Chauhan <amangotpro@gmail.com>
2023-06-11 14:30:41 +0530

    add: router

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { makeServer } from "./server";
+import { BrowserRouter as Router } from "react-router-dom";
 
 // Call make Server
 makeServer();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
+
   </React.StrictMode>,
 )
