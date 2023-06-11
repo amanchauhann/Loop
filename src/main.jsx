ba6819181d367c0e692a88458952fde3dc70005e
Aman Chauhan <amangotpro@gmail.com>
2023-06-12 01:18:52 +0530

    wrap: chakra

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,19 @@ import App from './App.jsx'
 import './index.css'
 import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraBaseProvider, ChakraProvider } from '@chakra-ui/react';
 
 // Call make Server
 makeServer();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ChakraProvider>
+      <Router>
+        <App />
+      </Router>
+    </ChakraProvider>
+
 
   </React.StrictMode>,
 )
