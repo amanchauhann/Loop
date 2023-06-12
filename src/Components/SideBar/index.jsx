b1c559bbea51fff27ac5a2d13f992fed02dc22f9
Aman Chauhan <amangotpro@gmail.com>
2023-06-12 14:49:36 +0530

    refactor: className

--- src/Components/SideBar/index.jsx
+++ src/Components/SideBar/index.jsx
@@ -5,10 +5,10 @@ const SideBar = () => {
     return (
         <div className="sidebar_container">
             <Flex direction={"column"} w={"5rem"}>
-                <div className="sidebar_item" align="center"><i class="fa-solid fa-house fa-xl"></i></div>
-                <div className="sidebar_item" align="center"><i class="fa-solid fa-compass fa-xl"></i></div>
-                <div className="sidebar_item" align="center"><i class="fa-solid fa-bookmark fa-xl"></i></div>
-                <div className="sidebar_item" align="center"><i class="fa-solid fa-heart fa-xl"></i></div>
+                <div className="sidebar_item" align="center"><i className="fa-solid fa-house fa-xl"></i></div>
+                <div className="sidebar_item" align="center"><i className="fa-solid fa-compass fa-xl"></i></div>
+                <div className="sidebar_item" align="center"><i className="fa-solid fa-bookmark fa-xl"></i></div>
+                <div className="sidebar_item" align="center"><i className="fa-solid fa-heart fa-xl"></i></div>
             </Flex>
 
         </div>
