5961a4f75abb4db72372f69cfb0675bcc12eefaf
Aman Chauhan <amangotpro@gmail.com>
2023-06-15 01:25:03 +0530

    add: layout_explore

--- src/Features/Explore/index.jsx
+++ src/Features/Explore/index.jsx
@@ -1,11 +1,22 @@
+import { Flex } from "@chakra-ui/react";
+import Nav from "../../Components/Nav";
 import { usePosts } from "../../Contexts/PostsProvider";
 import Post from "../PageFeed/Components/Post";
+import SideBar from "../../Components/SideBar";
+import AddPost from "../PageFeed/Components/AddPost";
+import PostsLayout from "../../Layout/PostsLayout";
 
 const Explore = () => {
     const { allPosts: { posts } } = usePosts()
     return (
         <>
-            {posts.map(eachPost => <Post key={eachPost._id} {...eachPost} />)}
+            <Nav />
+            <Flex>
+                <SideBar />
+                <Flex p={"10px"} maxW={"35rem"} direction={"column"} gap={"3rem"}>
+                    <PostsLayout children={posts.map(eachPost => <Post key={eachPost._id} {...eachPost} />)} />
+                </Flex>
+            </Flex>
         </>
     )
 }
