07e74fdf5ba01f0962eeef63557eae05907bc6e9
Aman Chauhan <amangotpro@gmail.com>
2023-06-16 23:54:28 +0530

    add: filter_buttons

--- src/Features/PageFeed/index.jsx
+++ src/Features/PageFeed/index.jsx
@@ -5,10 +5,14 @@ import AddPost from "./Components/AddPost"
 import Post from "./Components/Post"
 import { usePosts } from "../../Contexts/PostsProvider"
 import PostsLayout from "../../Layout/PostsLayout"
+import { Button } from "@chakra-ui/react"
+import { useState } from "react"
 
 const PageFeed = () => {
     const { allPosts: { feedPosts } } = usePosts()
+    const [selected_button, set_selected_button] = useState(null);
 
+    const handle_select = (e) => set_selected_button(e.target.value)
     return (
         <>
             <Nav />
@@ -16,6 +20,35 @@ const PageFeed = () => {
                 <SideBar />
                 <Flex p={"10px"} maxW={"35rem"} direction={"column"} gap={"3rem"}>
                     <AddPost />
+                    <Flex>
+                        <Button
+                            flex={1}
+                            bg={selected_button === "latest" ? "transparent" : "rgb(246, 226, 194)"}
+                            color={selected_button === "latest" ? "currentcolor" : "black"}
+                            border={selected_button === "latest" ? "1px solid currentcolor" : "none"}
+                            _hover={{ color: 'currentcolor', bg: "transparent", border: "1px solid currentcolor" }}
+                            _focus={{ outline: 'none' }}
+                            transition={"0.7s"}
+                            value={"latest"}
+                            onClick={handle_select}
+                        >
+                            <i class="fa-solid fa-clock fa-lg"></i>
+                            Latest
+                        </Button>
+                        <Button
+                            flex={1}
+                            bg={selected_button === "trending" ? "transparent" : "rgb(246, 226, 194)"}
+                            color={selected_button === "trending" ? "currentcolor" : "black"}
+                            border={selected_button === "trending" ? "1px solid currentcolor" : "none"}
+                            _hover={{ color: 'currentcolor', bg: "transparent", border: "1px solid currentcolor" }}
+                            _focus={{ outline: 'none' }}
+                            value={"trending"}
+                            onClick={handle_select}
+                        >
+                            <i class="fa-solid fa-fire fa-lg"></i>
+                            Trending
+                        </Button>
+                    </Flex>
                     <PostsLayout children={feedPosts.map(eachFeedPost => <Post key={eachFeedPost._id} {...eachFeedPost} />)} />
                 </Flex>
             </Flex>
