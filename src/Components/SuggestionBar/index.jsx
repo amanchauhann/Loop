5d79c7b4e99d41970b174a7d52a5736a6b9e8baa
Aman Chauhan <amangotpro@gmail.com>
2023-06-24 14:11:51 +0530

    style: breakpoint_suggestion

--- src/Components/SuggestionBar/index.jsx
+++ src/Components/SuggestionBar/index.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Button, Card, CardHeader, Flex, Heading, Text } from "@chakra-ui/react"
+import { Avatar, Box, Button, Card, CardHeader, Flex, Heading, Text, useBreakpointValue } from "@chakra-ui/react"
 import Chip from "./Chip"
 import { useAuth } from "../../Contexts/AuthProvider"
 import { useUsers } from "../../Contexts/UsersProvider"
@@ -8,13 +8,14 @@ const SuggestionBar = () => {
     const [users_without_currentUser, Set_users_without_currentUser] = useState([])
     const { allUsers: { users } } = useUsers()
     const { userData: { user: { user_details } } } = useAuth()
+    const shouldHide = useBreakpointValue({ base: true, md: false });
 
     useEffect(() => {
         Set_users_without_currentUser(users.filter(({ _id }) => _id !== user_details._id))
     }, [users])
 
     return (
-        <Flex direction={"column"} gap={"1rem"} border={"1px solid currentcolor"} p={"1rem"} h={"fit-content"}>
+        <Flex direction={"column"} gap={"1rem"} border={"1px solid currentcolor"} p={"1rem"} h={"fit-content"} display={shouldHide ? 'none' : 'flex'}>
             <Heading size={"sm"}>Suggestions for you</Heading>
             {users_without_currentUser.map(each_user => <Chip {...each_user} />)}
 
