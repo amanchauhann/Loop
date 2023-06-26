080fd5b1f695b9f69c1545f366ef3154cef6e036
Aman Chauhan <amangotpro@gmail.com>
2023-06-27 01:33:41 +0530

    add: link_to_profile_in_comment

--- src/Features/PostDetails/Components/Comment/index.jsx
+++ src/Features/PostDetails/Components/Comment/index.jsx
@@ -5,6 +5,7 @@ import { getSpecificUserService } from "../../../../Services/userServices"
 import { useState } from "react"
 import { useUsers } from "../../../../Contexts/UsersProvider"
 import { usePosts } from "../../../../Contexts/PostsProvider"
+import { Link } from "react-router-dom"
 
 const Comment = ({ username, text }) => {
     const [specific_user_profile, set_specific_user_profile] = useState({})
@@ -16,13 +17,16 @@ const Comment = ({ username, text }) => {
 
     return (
         <Flex style={divider_border} direction={"column"} gap={4} p={"10px"}>
-            <Flex gap={3}>
-                <Avatar src={specific_user_profile.displayImg} />
-                <Box>
-                    <Text fontWeight={500} fontSize={"md"}>{specific_user_profile.firstName} {specific_user_profile.lastName}</Text>
-                    <Text fontWeight={300} fontSize={"xs"}>@{username}</Text>
-                </Box>
-            </Flex>
+            <Link to={`/profile/${username}`}>
+                <Flex gap={3}>
+                    <Avatar src={specific_user_profile.displayImg} />
+                    <Box>
+                        <Text fontWeight={500} fontSize={"md"}>{specific_user_profile.firstName} {specific_user_profile.lastName}</Text>
+                        <Text fontWeight={300} fontSize={"xs"}>@{username}</Text>
+                    </Box>
+                </Flex>
+            </Link>
+
             <Text>{text}</Text>
         </Flex>
     )
