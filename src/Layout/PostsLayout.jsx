89a095ea326ff7671604e779f0a3acb1e9164b6c
Aman Chauhan <amangotpro@gmail.com>
2023-06-28 21:25:04 +0530

    style: width_posts_children

--- src/Layout/PostsLayout.jsx
+++ src/Layout/PostsLayout.jsx
@@ -2,7 +2,7 @@ import { Flex } from "@chakra-ui/react"
 
 const PostsLayout = ({ children }) => {
     return (
-        <Flex direction={"column"} gap={"2rem"}>
+        <Flex direction={"column"} gap={"2rem"} w={"35rem"} p={"10px"}>
             {children}
         </Flex>
     )
