fc4fdf7713f0665f048737ded4dbf70eb800cd5c
Aman Chauhan <amangotpro@gmail.com>
2023-06-30 23:01:10 +0530

    feature: search_with_debouncing

--- src/Components/Nav/index.jsx
+++ src/Components/Nav/index.jsx
@@ -1,21 +1,49 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./main.css"
 import { Link } from "react-router-dom";
 import { divider_border } from "../../Styles/Global";
+import { getUsersService } from "../../Services/userServices";
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+import { useAuth } from "../../Contexts/AuthProvider";
 
 const Nav = () => {
     const [search_value, setSearch_value] = useState("");
-    const search_value_handler = (e) => {
-        setSearch_value(e.target.value);
+    const [suggestions, set_suggestions] = useState([])
+
+    const { userData: { user: { user_details, encoded_token } } } = useAuth()
+
+    const fetch_users = async () => {
+        try {
+            const { data, status } = await getUsersService()
+            if (status === 200) {
+                return data
+            }
+        } catch (e) {
+            console.error("from nav_fetch_users", e)
+        }
     }
+
+
+    const timeout_ref = useRef(null)
+    const search_value_handler = (e) => {
+        const { value } = e.target
+        setSearch_value(value);
+        clearTimeout(timeout_ref.current)
+        timeout_ref.current = setTimeout(async () => {
+            const all_users = await fetch_users()
+            set_suggestions(all_users.users.filter(({ firstName }) => firstName.toLowerCase().includes(value)))
+        }, 500);
+    }
+
     return (
         <nav className="navigation" style={{ ...divider_border, position: "sticky", top: 0, zIndex: 1 }}>
-            <Link to={"/"} style={{ color: "currentcolor" }}>Loop</Link>
+            <Link to={"/"} className="current_color">Loop</Link>
 
-            <Link to={"/mockman"}>
+            {/* <Link to={"/mockman"}>
                 Mockman
-            </Link>
-            <div className="input_container">
+            </Link> */}
+
+            <Box position={"relative"} className="search_container">
                 <input
                     className="search_bar"
                     type="search"
@@ -23,7 +51,23 @@ const Nav = () => {
                     onChange={search_value_handler}
                     value={search_value}
                 />
-            </div>
+                {search_value.length > 0 &&
+                    <Box style={divider_border} bg={"rgb(23, 24, 34)"} color={"currentcolor"} position={"absolute"} w={'100%'} className={`suggestions`}>
+                        {suggestions.length > 0 ? suggestions?.map(each_suggestion =>
+                            <Link to={`/profile/${each_suggestion.username}`} className="current_color">
+                                <Flex style={divider_border} p={2} gap={2} key={each_suggestion._id}>
+
+                                    <Avatar src={each_suggestion?.displayImg} size={"md"} />
+                                    <Box>
+                                        <Text fontSize={"sm"}>{each_suggestion?.firstName} {each_suggestion?.lastName}</Text>
+                                        <Text fontSize={"sm"}>@{each_suggestion?.username}</Text>
+                                    </Box>
+
+
+                                </Flex>  </Link>) : <Text>No user found</Text>}
+                    </Box>
+                }
+            </Box>
         </nav>
     )
 }
