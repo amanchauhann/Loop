1c31039ef661f9916f2c0f264ba8f92868081287
Aman Chauhan <amangotpro@gmail.com>
2023-07-04 17:54:53 +0530

    fix: search

--- src/Components/Nav/index.jsx
+++ src/Components/Nav/index.jsx
@@ -42,24 +42,24 @@ const Nav = () => {
                 <input
                     className="search_bar"
                     type="search"
-                    placeholder="search here..."
+                    placeholder="search users..."
                     onChange={search_value_handler}
                     value={search_value}
                 />
-                {search_value.length > 0 &&
-                    <Box style={divider_border} bg={"rgb(23, 24, 34)"} color={"currentcolor"} position={"absolute"} w={'100%'} className={`suggestions`}>
-                        {suggestions.length > 0 ? suggestions?.map(each_suggestion =>
-                            <Link to={`/profile/${each_suggestion.username}`} className="current_color" key={each_suggestion._id}>
-                                <Flex style={divider_border} p={2} gap={2}>
+                {/* {search_value.length > 0 && */}
+                <Box style={divider_border} bg={"rgb(23, 24, 34)"} color={"currentcolor"} position={"absolute"} w={'100%'} className={`suggestions`}>
+                    {suggestions.length > 0 ? suggestions?.map(each_suggestion =>
+                        <Link to={`/profile/${each_suggestion.username}`} className="current_color" key={each_suggestion._id}>
+                            <Flex style={divider_border} p={2} gap={2}>
 
-                                    <Avatar name={`${each_suggestion.firstName} ${each_suggestion.lastName}`} src={each_suggestion?.displayImg} size={"md"} />
-                                    <Box>
-                                        <Text fontSize={"sm"}>{each_suggestion?.firstName} {each_suggestion?.lastName}</Text>
-                                        <Text fontSize={"sm"}>@{each_suggestion?.username}</Text>
-                                    </Box>
-                                </Flex>  </Link>) : <Text>No user found</Text>}
-                    </Box>
-                }
+                                <Avatar name={`${each_suggestion.firstName} ${each_suggestion.lastName}`} src={each_suggestion?.displayImg} size={"md"} />
+                                <Box>
+                                    <Text fontSize={"sm"}>{each_suggestion?.firstName} {each_suggestion?.lastName}</Text>
+                                    <Text fontSize={"sm"}>@{each_suggestion?.username}</Text>
+                                </Box>
+                            </Flex>  </Link>) : <Text>No user found</Text>}
+                </Box>
+                {/* } */}
             </Box>
             <Link to={`/profile/${user_details.username}`}>
                 <Avatar bg={"cyan"} name={`${user_details.firstName} ${user_details.lastName}`} src={user_details.displayImg} />
