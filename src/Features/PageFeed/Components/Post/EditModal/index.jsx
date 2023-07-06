0973bdea8b2f2d066565b072ed32ffd87daf5822
Aman Chauhan <amangotpro@gmail.com>
2023-07-07 00:15:39 +0530

    add: edit_image

--- src/Features/PageFeed/Components/Post/EditModal/index.jsx
+++ src/Features/PageFeed/Components/Post/EditModal/index.jsx
@@ -1,21 +1,29 @@
-import { Button, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Textarea } from "@chakra-ui/react"
+import { Box, Button, Image, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, Textarea } from "@chakra-ui/react"
 import { useState } from "react"
 import { getEditPostService } from "../../../../../Services/postServices"
 import { usePosts } from "../../../../../Contexts/PostsProvider"
 import { POSTS } from "../../../../../Common/reducerTypes"
-import { errorToast } from "../../../../../utils"
+import { errorToast, upload_image } from "../../../../../utils"
 
-const EditModal = ({ isOpen, onClose, content, _id, encoded_token }) => {
+const EditModal = ({ isOpen, onClose, content, _id, encoded_token, media }) => {
     const { postsDispatch, allPosts: { posts, feedPosts } } = usePosts()
     const [edit_content, set_edit_content] = useState(`${content}`)
+    const [edit_post, set_edit_post] = useState({
+        content: `${content}`,
+        media: `${media ? media : ""}`
+    })
+    const [media_uploading, set_media_uploading] = useState(false)
 
     const edit_value_handler = (e) => {
-        set_edit_content(e.target.value)
+        set_edit_post(prev => ({
+            ...prev,
+            content: e.target.value
+        }))
     }
     const edit_post_handler = (id, encodedToken) => {
-        const edit_post = async () => {
+        const edit_post_update = async () => {
             try {
-                const { data: { posts }, status } = await getEditPostService(edit_content, id, encodedToken)
+                const { data: { posts }, status } = await getEditPostService(edit_post, id, encodedToken)
                 if (status === 201) {
                     postsDispatch({ type: POSTS.INITIALISE, payload: posts })
                     onClose()
@@ -26,7 +34,24 @@ const EditModal = ({ isOpen, onClose, content, _id, encoded_token }) => {
                 errorToast(`${e.response.status}: ${e.response.data.errors}`)
             }
         }
-        edit_post()
+        edit_post_update()
+    }
+
+    const onUploadClick = async (e) => {
+        const selected_file = e.target.files[0]
+        if (selected_file) {
+            try {
+                set_media_uploading(true)
+                const img_url = await upload_image(selected_file)
+                if (img_url) {
+                    set_edit_post(prev => ({ ...prev, media: img_url.url }))
+                    set_media_uploading(false)
+                }
+            } catch (e) {
+                console.error("from onUploadClick", e)
+                errorToast(`${e?.response?.status}: ${e?.response?.data?.errors}`)
+            }
+        }
     }
 
     return (
@@ -37,7 +62,37 @@ const EditModal = ({ isOpen, onClose, content, _id, encoded_token }) => {
                     <ModalHeader>Edit your Post</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody pb={6}>
-                        <Textarea resize="vertical" value={edit_content} onChange={edit_value_handler} />
+                        <Textarea resize="vertical" value={edit_post.content} onChange={edit_value_handler} />
+                        <Box>
+                            <Text>Media:</Text>
+                            {media_uploading ? <Text>UPLOADING</Text> : edit_post.media && <Box position={"relative"} w={"fit-content"}>
+                                <Image boxSize={"100px"} src={edit_post.media} />
+                                <Text onClick={
+                                    () =>
+                                        set_edit_post(prev => ({
+                                            ...prev,
+                                            media: ""
+                                        }))
+                                } position={"absolute"} top={-3} right={-2}><i className="fa-sharp fa-regular fa-circle-xmark fa-lg"></i></Text>
+                            </Box>}
+
+                            <div style={{ position: "relative", marginTop: "10px" }}>
+                                <i className="fa-regular fa-image fa-lg" >
+                                    <Input
+                                        position={"absolute"}
+                                        top={0}
+                                        left={0}
+                                        height={5}
+                                        cursor={"pointer"}
+                                        type="file"
+                                        opacity={0}
+                                        accept="image/*"
+                                        onChange={onUploadClick}
+                                    />
+                                </i>
+                            </div>
+                        </Box>
+
                     </ModalBody>
 
                     <ModalFooter>
